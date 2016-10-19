d342d5a6cbc6efab03d7c66150917a997b9a71e7
fran-bravo <fran_ase@hotmail.com>
2016-10-19 18:12:25 -0300

    Setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='pylogic',
     packages=['pylogic'],
-    version='0.1.4',
+    version='0.1.5',
     description='First description for the module',
     author='Francisco Bravo',
     author_email='fran_ase@hotmail.com',
     license='MIT',
     url='https://github.com/fran-bravo/pylogic-module',
-    download_url='https://github.com/fran-bravo/pylogic-module/tarball/0.1.4',
+    download_url='https://github.com/fran-bravo/pylogic-module/tarball/0.1.5',
     keywords=['testing', 'logic'],
     classifiers=['Development Status :: 3 - Alpha', ],
 )
